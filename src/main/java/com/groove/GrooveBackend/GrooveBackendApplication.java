298f882dcafc70df96e5e638abaf45f40576f6bc
Soumik Dutta <shalearkane@gmail.com>
2022-09-19 15:46:24 +0530

    created spring project from start.spring.io

---
+++ src/main/java/com/groove/GrooveBackend/GrooveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.groove.GrooveBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrooveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groove/GrooveBackend/GrooveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groove.GrooveBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrooveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
