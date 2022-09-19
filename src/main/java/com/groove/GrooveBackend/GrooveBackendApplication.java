138137bfe60790094c300b659e71cab98f8226fd
Soumik Dutta <shalearkane@gmail.com>
2022-09-19 17:16:07 +0530

    added hello world

--- src/main/java/com/groove/GrooveBackend/GrooveBackendApplication.java
+++ src/main/java/com/groove/GrooveBackend/GrooveBackendApplication.java
@@ -2,12 +2,28 @@ package com.groove.GrooveBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GrooveBackendApplication {
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().antMatchers("/hello", "/ignore2");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrooveBackendApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
 
 }
